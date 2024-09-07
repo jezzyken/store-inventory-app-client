c671e4ccd04baa9f55a4038f5174aff9b6a44b40
jezrelljolampong <jezrelljolampong17@gmail.com>
2024-09-07 07:43:59 -0600

    updates

---
+++ src/store/modules/product.js
@@ -0,0 +1,91 @@
+/* eslint-disable */
+
+import axios from "axios";
+
+const url = require("@/config").url;
+
+const state = {
+  items: [],
+};
+
+const endpoint = "products";
+
+const actions = {
+  async getItem({ commit }, data) {
+    try {
+      const response = await axios.get(`${url}/${endpoint}`);
+      return response.data;
+    } catch (error) {
+      return error.response;
+    }
+  },
+
+  async getItems({ commit }) {
+    try {
+      const response = await axios.get(`${url}/${endpoint}/items`);
+      console.log(response)
+      return response.data;
+    } catch (error) {
+      return error.response;
+    }
+  },
+
+  async getItemById({ commit }, id) {
+    try {
+      const response = await axios.get(`${url}/${endpoint}/${id}`);
+      return response.data;
+    } catch (error) {
+      return error.response;
+    }
+  },
+
+  async getItemPrices({ commit }, id) {
+    try {
+      const response = await axios.get(`${url}/${endpoint}/${id}/prices`);
+      return response.data;
+    } catch (error) {
+      return error.response;
+    }
+  },
+
+  async addItem({ commit }, data) {
+    try {
+      const response = await axios.post(`${url}/${endpoint}`, data);
+      return response.data;
+    } catch (error) {
+      console.log(error)
+      return error.response;
+    }
+  },
+
+  async updateItem({ commit }, data) {
+    try {
+      const response = await axios.put(`${url}/${endpoint}/${data.id}`, data.data);
+      return response.data;
+    } catch (error) {
+      return error.response;
+    }
+  },
+
+  async deleteItem({ commit }, id) {
+    try {
+      const response = await axios.delete(`${url}/${endpoint}/${id}`);
+      return response.data;
+    } catch (error) {
+      return error.response;
+    }
+  },
+};
+
+const mutations = {
+  SET_ITEM(state, data) {
+    state.items = data;
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  actions,
+  mutations,
+};
